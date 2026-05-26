b344ed692c26a742faa684e932733244ee88882a
Jozef <dzeykop@gmail.com>
2026-05-26 22:16:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ echo "=== Aufgaben-Dashboard Installation ==="
 apt update && apt install -y python3 python3-pip python3-venv git
 
 cd /opt
-git clone https://github.com/dzeykop/aufgaben-dashboard.git
-cd aufgaben-dashboard
+git clone https://github.com/dzeykop/taskboard.git
+cd taskboard
 
 python3 -m venv venv
 source venv/bin/activate
