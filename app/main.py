0667ecc227bce74a5649163778c161d61e53f9d4
Jozef <dzeykop@gmail.com>
2026-05-27 21:50:55 +0200

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_login import LoginManager
+from dotenv import load_dotenv
+
+load_dotenv('/opt/taskboard/.env')
+
+app = Flask(__name__)
+app.secret_key = os.getenv('FLASK_SECRET', 'fallback_secret')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+from routes import *
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
