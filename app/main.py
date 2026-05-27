f06068c231caea2821bba543d99e8418f9ad7b13
Jozef <dzeykop@gmail.com>
2026-05-27 22:03:55 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 import os
+import sys
+sys.path.insert(0, '/opt/taskboard/bot')
+
 from flask import Flask
 from flask_login import LoginManager
 from dotenv import load_dotenv
@@ -12,7 +15,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from routes import *
+from routes import register_routes
+register_routes(app, login_manager)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
